a156e071c748ab8da4a1a6e2382fdb87d6b3c895
loveclari <clarissa.celestino@gmail.comm>
2022-08-12 18:01:01 -0700

    adding and remove other job selection

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-//adding focus on name input on load
+// gloabl variables
 
-let inputFocus = document.querySelector("input#name");
-window.addEventListener('load', (e) =>{
+let inputFocus = document.querySelector('input#name');
+let select = document.getElementById('title');
+let OtherJobRole = document.querySelector('input#other-job-role');
+let otherJob = select.options[6].value;
+
+console.log(otherJob)
+
+// adding focus on name input on load
+
+window.addEventListener('load', (e) => {
     inputFocus.focus();
+    OtherJobRole.style.display = "none"
 })
+
+// show job input based on select
+
+select.addEventListener('change', () => {
+    if(otherJob) {
+        OtherJobRole.style.display = "block"
+    } else {
+        OtherJobRole.style.display = "none"
+    }
+    
+})
+
+
