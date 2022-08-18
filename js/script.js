9e3f0e54aedade119f5ad4f292fcfe5ec7631626
loveclari <clarissa.celestino@gmail.comm>
2022-08-17 22:38:54 -0700

    showing select list based on another selection

--- js/script.js
+++ js/script.js
@@ -2,26 +2,19 @@
 
 let inputFocus = document.querySelector('input#name');
 let selectJob = document.getElementById('title');
-let selectDesign = document.getElementById('design');
 let selectColor = document.getElementById('color');
 let designColor = document.querySelector('#shirt-colors');
 let OtherJobRole = document.querySelector('input#other-job-role');
-// let jsPuns = document.querySelectorAll('[data-theme="js puns"]');
-// let heartJs = document.querySelectorAll('[data-theme="heart js"]');
-
+let selectDesign = document.querySelector('select[id=design]')
 let otherJob = selectJob.options[6].value;
-let jsPunstheme = selectDesign.options[1].value;
-let heartJstheme = selectDesign.options[2].value;
 
-console.log(jsPunstheme)
-console.log(heartJstheme)
 
 // adding focus on name input on load
 
 window.addEventListener('load', (e) => {
     inputFocus.focus();
-    OtherJobRole.style.display = "none"
-    designColor.style.display = "none"
+    OtherJobRole.style.display = "none";
+    designColor.style.display = "none";
 });
 
 // prevent submit from loading the page
@@ -45,16 +38,34 @@ selectJob.addEventListener('change', (event) => {
 
 
 selectDesign.addEventListener('change', (event) => {
-    let jsPuns = document.querySelectorAll('[data-theme="js puns"]');
-    let heartJs = document.querySelectorAll('[data-theme="heart js"]');
-    if ( event.target === jsPunstheme){
-        jsPuns.style.display = "block";
-      } else if( event.target === heartJstheme ){
-        heartJs.style.display = "block";
-      } 
-      console.log(jsPuns)
-      console.log(heartJs)
+
+    designColor.style.display = "block"
+
+    let selected = event.target.options[event.target.selectedIndex]
+    let theme = selected.value
+
+    // Getting all options on the array
+
+    let options = Array.from(document.querySelectorAll('select[id=color]>option'))
+
+    // Hide items that are not appart of the group
+    
+    options.forEach((element) => {
+
+        if(element.getAttribute('data-theme') == theme){
+            element.style.display = "block"
+        } else if(element.getAttribute('data-theme') !== theme){
+            element.style.display = "none"
+        }
+
+    });  
     
 });
 
 
+
+
+
+
+// Resources:
+// https://stackoverflow.com/questions/54429553/how-to-show-hide-items-from-a-select-list-based-on-another-selection
