b0ab5b18f7b1865ee431a059ee1cb1d69f2da6ce
loveclari <clarissa.celestino@gmail.comm>
2022-08-23 10:24:03 -0700

    adding event listener for focus on checkbox

--- js/script.js
+++ js/script.js
@@ -16,6 +16,14 @@ let creditcard = document.querySelector('#credit-card');
 let paypal = document.querySelector('#paypal');
 let bitcoin = document.querySelector('#bitcoin');
 let paymentHideFirst = payment.options[0];
+let submit = document.querySelector('[type=submit]');
+
+let ccInput = document.getElementById('cc-num');
+let emailInput = document.getElementById('email');
+let zipInput = document.getElementById('zip');
+let cvvInput = document.getElementById('cvv');
+
+
 
 
 // adding focus on name input on load
@@ -24,7 +32,9 @@ window.addEventListener('load', () => {
     inputFocus.focus();
     OtherJobRole.style.display = "none";
     designColor.style.display = "none";
-    paymentHideFirst.parentNode.removeChild(paymentHideFirst)
+    paymentHideFirst.parentNode.removeChild(paymentHideFirst);
+    paypal.style.display = "none";
+    bitcoin.style.display = "none";
     
 });
 
@@ -50,10 +60,10 @@ selectJob.addEventListener('change', (event) => {
 
 selectDesign.addEventListener('change', (event) => {
 
-    designColor.style.display = "block"
+    designColor.style.display = "block";
 
-    let selected = event.target.options[event.target.selectedIndex]
-    let theme = selected.value
+    let selected = event.target.options[event.target.selectedIndex];
+    let theme = selected.value;
 
     // Getting all options on the array
 
@@ -104,6 +114,19 @@ activityBox.addEventListener('change', () => {
 
 });
 
+// adding class for focus depending on event
+
+
+
+checkbox.forEach((checkbox) => {
+    checkbox.addEventListener("focus", (event) => {
+      event.target.parentElement.classList.add("focus");
+    });
+    checkbox.addEventListener("blur", (event) => {
+      event.target.parentElement.classList.remove("focus");
+    });
+});
+
 // create an event listener to show the user only the payment features he selected
 
 
@@ -127,7 +150,80 @@ payment.addEventListener('change', (event) => {
 });
 
 
+// form validation
+
+const nameValidation = (name) => {
+    return /^[a-z]+$/.test(name);
+
+}
+
+const emailValidation = (email) => {
+    return /^$|^.*@.*\..*$/.test(email);
+
+}
+
+
+const cvvValidation = (cvv) => {
+    return /^[0-9]{3,4}$/.test(cvv)
+
+}
+
+const zipValidation = (zip) => {
+    return /^[0-9]{5}$/.test(zip)
+
+}
+
+const ccValidation = (ccNum) => {
+    return /^[0-9]{13,16}$/.test(ccNum)
+}
+
+// helper functions
+
+
+function showOrHideTip(show, element) {
+    // show element when show is true, hide when false
+    if (show) {
+      element.style.display = "inherit";
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
+  function createListener(validator) {
+    return event => {
+      const text = event.target.value;
+      const valid = validator(text);
+      const showTip = text !== "" && !valid;
+      const tooltip = event.target.nextElementSibling;
+      showOrHideTip(showTip, tooltip);
+    };
+  }
+
+
+// adding event listeners for form validators
+
+emailInput.addEventListener("input", createListener(emailValidation));
+
+inputFocus.addEventListener("input", createListener(nameValidation));
+
+cvvInput.addEventListener("input", createListener(cvvValidation));
+
+zipInput.addEventListener("input", createListener(zipValidation));
+
+ccInput.addEventListener("input", createListener(ccValidation));
+
+// adding event listener to the form submit
+
+submit.addEventListener('submit', (event) => {
+    console.log(event.target)
+
+})
+
+
 
 
 // Resources:
 // https://stackoverflow.com/questions/54429553/how-to-show-hide-items-from-a-select-list-based-on-another-selection
+// https://cheatography.com/davechild/cheat-sheets/regular-expressions/
+// https://stackoverflow.com/questions/71538996/get-id-of-child-elements
+// http://www.madirish.net/11
